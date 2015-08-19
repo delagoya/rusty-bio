a168f57a1060d3120a595cbbc09cf119928f0d63
Angel Pizarro <delagoya@gmail.com>
2015-08-19 19:39:59 -0400

    really del artifacts

--- sources/rust/getting_started/guessing_game/target/debug/guessing_game.d
+++
@@ -1,4 +0,0 @@
-/Users/pizarroa/Dropbox/src/rusty-bio/sources/getting_started/guessing_game/target/debug/guessing_game: src/main.rs
-
-/Users/pizarroa/Dropbox/src/rusty-bio/sources/getting_started/guessing_game/target/debug/guessing_game.d: src/main.rs
-
